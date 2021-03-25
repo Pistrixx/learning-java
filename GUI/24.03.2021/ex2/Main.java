7a59476a8e79717328c2bb767f3a2c651abf265f
Pistrixx <59702043+Pistrixx@users.noreply.github.com>
2021-03-25 16:10:58 +0100

    Create Main.java

---
+++ GUI/24.03.2021/ex2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws TooBig {
+
+        Square square = new Square(4);
+
+        System.out.println(square.getArea());
+        System.out.println(square.getPerimeter());
+
+    }
+}
